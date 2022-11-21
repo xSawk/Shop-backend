2fc3ee8d08880d4a2bbe6cd642e74dee80daa4a8
xSawk <marcinlukasik1997@gmail.com>
2022-11-21 13:32:51 +0100

    Initial commit

---
+++ src/main/java/pl/lukasik/shop/ShopBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.lukasik.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/lukasik/shop/ShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lukasik.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
